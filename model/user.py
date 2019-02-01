d5f518ae11c55b6c15a97c66cc8f382a508021f8
dbulka <dzm.bulka@gmail.com>
2019-02-01 22:55:17 +0300

    2nd commit

--- model/user.py
+++ model/user.py
@@ -2,6 +2,16 @@ class User:
 
     def __init__(self, *args, **kwargs):
 
-    def fill_text_bottom(self, *args, **kwargs)
+    def log_in(self, *args, **kwargs)
 
-    def admin_access(self, *args, **kwargs)
+    def log_out(self, *args, **kwargs)
+
+    def check_person_info(self, *args, **kwargs)
+
+    def check_in(self, *args, **kwargs)
+
+    def register(self, *args, **kwargs)
+
+    def search_flight(self, *args, **kwargs)
+
+    def order_ticket(self, *args, **kwargs)
