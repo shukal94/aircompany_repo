5ebe6b017e1204993b66818a6582186227c2ea09
dbulka <dzm.bulka@gmail.com>
2019-02-02 00:16:05 +0300

    'com'

---
+++ model/user.py
@@ -0,0 +1,17 @@
+class User:
+
+    def __init__(self, *args, **kwargs):
+
+    def log_in(self, *args, **kwargs)
+
+    def log_out(self, *args, **kwargs)
+
+    def check_person_info(self, *args, **kwargs)
+
+    def check_in(self, *args, **kwargs)
+
+    def register(self, *args, **kwargs)
+
+    def search_flight(self, *args, **kwargs)
+
+    def order_ticket(self, *args, **kwargs)
