7791b7791aeb7562eb62a63e3ad488f62a462d83
timtimbot <timtimbot@gmail.com>
2019-03-21 20:30:44 +0300

    added users methods

--- app/api/users.py
+++ app/api/users.py
@@ -14,6 +14,14 @@ def get_user(id):
         id=user.id,
         username=user.username,
         role=user.role.name,
+        postal_code=user.postal_code,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        address=user.address,
+        state=user.state,
+        country=user.country,
+        email=user.email,
+        about_me=user.about_me,        
         flights=[{
                     'from': flight._from,
                     'to': flight._to,
@@ -39,27 +47,68 @@ def get_users():
 @bp.route('/users', methods=['POST'])
 def create_user():
     data = request.get_json(force=True)
-    user = User(
+    User(
         username=data["username"],
         first_name=data["first_name"],
         last_name=data["last_name"],
         address=data["address"],
-        postal_code=data["postal_code"]
+        postal_code=data["postal_code"],
+        email=data["email"],
+        country=data["country"],
+        state=data["state"],
+        about_me=data["about_me"],
+        role_id=data["role_id"],
     )
     db.session.add(user)
     db.session.commit()
     user = User.query.filter_by(username=user.username).first()
     return jsonify(
         id=user.id,
-        username=user.username
+        username=user.username,
+        postal_code=user.postal_code,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        address=user.address,
+        state=user.state,
+        country=user.country,
+        email=user.email,
+        about_me=user.about_me,
+        role_id=user.role_id
     )
 
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
-    pass
-
+    data = request.get_json(force=True)
+    User.query.filter_by(id=id).update({User.postal_code : data["postal_code"],
+    User.username : data["username"],
+    User.first_name : data["first_name"],
+    User.last_name : data["last_name"],
+    User.address : data["address"],
+    User.state : data["state"],
+    User.country : data["country"],
+    User.email : data["email"],
+    User.about_me : data["about_me"],
+    User.role_id : data["role_id"]})
+    db.session.commit()
+    user = User.query.filter_by(id=id).first()
+    return jsonify(
+        id=user.id,
+        username=user.username,
+        postal_code=user.postal_code,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        address=user.address,
+        state=user.state,
+        country=user.country,
+        email=user.email,
+        about_me=user.about_me,
+        role_id=user.role_id
+    )
 
 @bp.route('/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
-    pass
+    User.query.filter_by(id=id).delete()
+    return jsonify(
+        message="success"
+    )
